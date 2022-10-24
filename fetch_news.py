c0057b09b020d2ec0ebe2a8beb9ae7d458063e49
Luna McNulty <l.mcnulty@riseup.net>
2022-10-24 17:24:06 -0400

    Refactor

---
+++ fetch_news.py
@@ -0,0 +1,208 @@
+MOCK = True
+keywords = [
+    ' AI ',
+    'artificial intelligence',
+    'machine learn',
+    'deep learning',
+    'facial recognition',
+    'face detection',
+    'object detection',
+    'object recognition',
+    'computer vision',
+    'self driving',
+    'self-driving',
+    'autonomous vehicle',
+    'autonomous driving',
+    'algorithmic bias',
+    'algorithmic fairness',
+    'biased algorithm',
+    'language model',
+    'neural net',
+    'language processing',
+    'NLP',
+]
+
+def main():
+    MONGODB_URI = environ.get('MONGODB_CONNECTION_STRING')
+  
+    print("Fetching news...")
+  
+    with open('feeds.txt', 'r') as f:
+        feed_urls = f.readlines()
+  
+  
+    m = np.array([0] * 768)
+  
+    mongo_client = None
+    incidents_collection = None
+    candidates_collection = None
+    if MONGODB_URI:
+        mongo_client = MongoClient(MONGODB_URI)
+        incidents_collection = mongo_client['aiidprod'].incidents
+        candidates_collection = mongo_client['aiidprod'].candidates
+  
+        for offset, incident in enumerate(incidents_collection.find(
+            { 'embedding': { '$exists': True } }, 
+            { 'embedding': { 'vector': True } }
+        )):
+            i = offset + 1
+  
+            # As the number of vectors contributing to the mean increases,
+            # the contribution of each one decreases:
+            #
+            # i=1 → m = (0/1)m + (1/1)v
+            # i=2 → m = (1/2)m + (1/2)v
+            # i=3 → m = (1/3)m + (2/3)v
+            # i=4 → m = (3/4)m + (1/4)v
+            # ...
+            v = np.array(incident['embedding']['vector'])
+            m = ( v * 1 / i) + (m * (i - 1) / i)
+  
+    mean_embedding = m
+        
+    for feed_url in feed_urls:
+        feed = feedparser.parse(feed_url)
+        for entry in feed['entries']:
+            process_url(
+                entry['link'], 
+                mean_embedding, 
+                mongo_client, 
+                candidates_collection
+            )
+  
+def process_url(article_url, mean_embedding, mongo_client, candidates_collection):
+    stemmer = PorterStemmer()
+    try:
+        
+        print("\nFetching", article_url)
+
+        if mongo_client != None:
+            result = candidates_collection.find_one({ 'url': article_url })
+            if result is not None:
+                print('URL already processed. Skipping...')
+                return
+
+        article = get_article(article_url)
+
+        if not article['date_published']:
+            return
+
+        print(article['title'])
+        print(article['date_published'])
+
+        article_words = [
+            stemmer.stem(word).lower() for word in
+            word_tokenize(article['plain_text'])
+        ]
+
+        matching_keywords = [ 
+            word.strip() for word in keywords 
+            if stemmer.stem(word).lower() in ' '.join(article_words)
+        ]
+        
+        print('Matching Keywords:', matching_keywords)
+
+        article['matching_keywords'] = matching_keywords
+
+
+        if (len(matching_keywords) > 0):
+            print('Running NLP...')
+            aws_root = 'https://q3z6vr2qvj.execute-api.us-west-2.amazonaws.com';
+            
+            nlp_response = None
+
+            if MOCK:
+                with open('mock.json') as f: 
+                    nlp_response = json.load(f) 
+            else:
+                # nlp_response = requests.post(
+                #     aws_root + '/text-to-embed',
+                #     data = {
+                #         'text': article['plain_text']
+                #     }
+                # )
+                nlp_response = requests.get(
+                    (
+                        aws_root + '/text-to-embed?' +
+                        urllib.parse.urlencode({
+                            'text': article['plain_text'],
+                        })
+                    )[0:2048],
+                    timeout=10
+                ).json()
+
+            article['embedding'] = nlp_response['body']['embedding']
+
+            if not all(mean_embedding == 0):
+                article['similarity'] = cosine_similarity(
+                    article['embedding']['vector'],
+                    mean_embedding
+                )
+                print(article['similarity'])
+
+                article['match'] = True
+
+                if mongo_client:
+                    print('Uploading to MongoDB...')
+                    candidates_collection.insert_one(article)
+        else:
+            if mongo_client:
+                print('Uploading to MongoDB...')
+                candidates_collection.insert_one({ 
+                    'match': False, 
+                    'url': article['url'], 
+
+                    # Keeping the title will help us see
+                    # if things that should be accepted are rejected.
+                    'title': article['title']
+                })
+            
+
+    except Exception as e:
+        print(e)
+        return
+    
+
+
+def get_article(article_url):
+
+    plaintext_reader = HTML2Text()
+    plaintext_reader.ignore_links = True
+    plaintext_reader.ignore_images = True
+
+    article_response = requests.get(article_url, timeout=10)
+
+    article_doc = Document(article_response.text)
+
+    summary = article_doc.summary()
+
+    date_published = htmldate.find_date(article_response.text)
+
+    return {
+        'title': article_doc.title(),
+        'text': html2text(summary),
+        'plain_text': plaintext_reader.handle(summary),
+        'url': article_url,
+        'date_published': date_published
+    }
+
+cosine_similarity = lambda a, b: np.dot(a, b) / (norm(a) * norm(b))
+
+
+
+
+import feedparser
+import requests
+import urllib
+import json
+import numpy as np
+import htmldate
+from pymongo import MongoClient
+from html2text import html2text, HTML2Text
+from readability import Document
+from os import environ
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from numpy.linalg import norm
+
+if __name__ == "__main__": main()
